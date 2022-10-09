d7a149146ebbebe9a168229e3e59eb76eb23f12a
William Price <wbprice3@yahoo.com>
2022-10-09 10:12:49 -0500

    Enabled Write to File for new Employees and Tickets

--- src/main/java/com/rev/project1/TheBrains.java
+++ src/main/java/com/rev/project1/TheBrains.java
@@ -1,5 +1,13 @@
 package com.rev.project1;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,6 +21,8 @@ import io.javalin.http.Context;
 
 public class TheBrains {
 
+	public static String recEmp;
+	public static String recTick;
 	public static void main(String[] args) {
 		
 		Javalin app = Javalin.create().start(8000);
@@ -32,7 +42,14 @@ public class TheBrains {
 			 * 
 			 */
 			Tickets receivedTicket = ctx.bodyAsClass(Tickets.class);
+			recTick = receivedTicket.toString();
 			System.out.println(receivedTicket);
+			
+			try (
+					BufferedWriter myWriter = Files.newBufferedWriter(Path.of("tickets.txt"), StandardOpenOption.APPEND)){myWriter.write(recTick + "\n");
+					}catch (IOException e) {
+						e.printStackTrace();
+					}
 			ctx.status(HttpStatus.CREATED_201);
 		});
 		
@@ -45,11 +62,39 @@ public class TheBrains {
 			 * the body into a Java object.
 			 * 
 			 */
+			
 			Employee receivedEmployee = ctx.bodyAsClass(Employee.class);
 			System.out.println(receivedEmployee);
+			recEmp = receivedEmployee.toString();
+			
+		//	File myFile = new File("employees.txt");
+			
+/*			try (FileWriter fileWriter = new FileWriter(myFile);BufferedWriter writer = new BufferedWriter(fileWriter)){
+			//FileOutputStream fos = new FileOutputStream(myFile); //deals in bytes
+			 //deals in characters
+			; //Deals in lines
+			
+			
+			writer.write(recEmp);
+			
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+*/			
+			
+			try (
+					BufferedWriter myWriter = Files.newBufferedWriter(Path.of("employees.txt"), StandardOpenOption.APPEND)){myWriter.write(recEmp + "\n");
+					}catch (IOException e) {
+						e.printStackTrace();
+					}
 			ctx.status(HttpStatus.CREATED_201);
+		
 		});
-	
+		
+		
+		
 	}//End of MAIN
-
+	
 }//End of Class
