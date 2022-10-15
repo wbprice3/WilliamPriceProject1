528671f80a6d8c61181622dda0dae8b431a1bf4f
William Price <wbprice3@yahoo.com>
2022-10-15 15:21:32 -0500

    Few small updates to ticket searching

--- src/main/java/com/revature/repository/TicketRepository.java
+++ src/main/java/com/revature/repository/TicketRepository.java
@@ -53,7 +53,7 @@ public class TicketRepository {
 				
 				stmt = conn.createStatement();
 
-				set = stmt.executeQuery("SELECT * FROM ticket");
+				set = stmt.executeQuery("SELECT * FROM ticket WHERE ticket_status = 'Pending'");
 				while (set.next()) {
 					ticklist.add(
 							new Tickets (set.getInt(1), set.getFloat(2), set.getString(3), set.getString(4), set.getString(5)));
